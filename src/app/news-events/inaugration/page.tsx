e6c43332ef1163fccdc9bfa040e0b1a754ff7da2
Reuel Nixon <reuelnixon@gmail.com>
2025-03-06 13:46:37 +0530

    chore: removed image optimizations

--- src/app/news-events/inaugration/page.tsx
+++ src/app/news-events/inaugration/page.tsx
@@ -1,19 +1,15 @@
 'use client';
 
 import Image from 'next/image';
-import Link from 'next/link';
-
-
-import { BgPattern } from '@/components/bg-pattern';
 
 export default function InaugrationPage() {
   const inaugrationGrid = [
-    { key:1, src:"/news-events/inaugration/1.JPG" },
-    { key:2, src:"/news-events/inaugration/2.JPG" },
-    { key:3, src:"/news-events/inaugration/3.JPG" },
-    { key:4, src:"/news-events/inaugration/4.JPG" },
-    { key:5, src:"/news-events/inaugration/5.JPG" },
-    { key:6, src:"/news-events/inaugration/6.jpg" },
+    { key: 1, src: '/news-events/inaugration/1.JPG' },
+    { key: 2, src: '/news-events/inaugration/2.JPG' },
+    { key: 3, src: '/news-events/inaugration/3.JPG' },
+    { key: 4, src: '/news-events/inaugration/4.JPG' },
+    { key: 5, src: '/news-events/inaugration/5.JPG' },
+    { key: 6, src: '/news-events/inaugration/6.jpg' },
   ];
 
   return (
@@ -32,35 +28,36 @@ export default function InaugrationPage() {
       {/* Info Section */}
       <section className='bg-[#f7f7f7] px-5 py-10'>
         <div className='container mx-auto text-center'>
-          <h2 className='mb-4 font-serif text-2xl md:text-3xl font-light text-[#333]'>Vice President Jagdeep Dhankhar inaugurated<br/> The Kulish School located in Jagatpura in the capital Jaipur</h2>
+          <h2 className='mb-4 font-serif text-2xl font-light text-[#333] md:text-3xl'>
+            Vice President Jagdeep Dhankhar inaugurated
+            <br /> The Kulish School located in Jagatpura in the capital Jaipur
+          </h2>
           {/* <h3 className='mb-5 text-xl font-normal text-[#555]'>Kulish Community Service Program</h3> */}
           <p className='mx-auto w-[85%] text-sm leading-relaxed text-[#666]'>
-          Hon'ble Vice President, Shri Jagdeep Dhankhar and Dr. Sudesh Dhankhar visited various facilities of The Kulish School and interacted with students in Jaipur, Rajasthan
+            Hon'ble Vice President, Shri Jagdeep Dhankhar and Dr. Sudesh Dhankhar visited various
+            facilities of The Kulish School and interacted with students in Jaipur, Rajasthan
           </p>
         </div>
       </section>
 
       {/* Cards Section */}
-      <div className='container mx-auto p-4 w-10/12 md:py-12'>
-            <div className='grid grid-cols-1 gap-8 '>
-      
-                  <div className='grid grid-cols-2 gap-4 md:grid-cols-3'>
-                  {inaugrationGrid.map((imgMap, index) =>(
-                  <div className='relative grid-cols-3 gap-4 overflow-hidden' >
-                      <Image
-                        src={imgMap.src}
-                        alt='Inaugration Grid'
-                        key={index}
-                        width={200}
-                        height={200}
-                        className='h-[200px] w-[200px] md:h-[400px] md:w-[400px] object-cover'
-                      />
-                    </div>
-                    )
-                  )}
-                  </div>
-                </div>
-            </div>
+      <div className='container mx-auto w-10/12 p-4 md:py-12'>
+        <div className='grid grid-cols-1 gap-8'>
+          <div className='grid grid-cols-2 gap-4 md:grid-cols-3'>
+            {inaugrationGrid.map((imgMap, index) => (
+              <div className='relative grid-cols-3 gap-4 overflow-hidden' key={index}>
+                <img
+                  src={imgMap.src}
+                  alt='Inaugration Grid'
+                  width={200}
+                  height={200}
+                  className='h-[200px] w-[200px] object-cover md:h-[400px] md:w-[400px]'
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
