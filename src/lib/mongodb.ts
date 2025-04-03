ab44eaf0ba9a87f7939f452a59830c762863d605
Mocktic Joshi <MOCKTIC.JOSHI@microchip.com>
2025-04-03 18:21:57 +0530

    feat: new careers page

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,7 +1,7 @@
 import { MongoClient } from 'mongodb';
 
 //keeping it here to resolve deploy errors:
-const uri: string = 'mongodb+srv://mockticjj:IUxUbt33TRdvovIj@kulishschool.vjr5xhq.mongodb.net/kulish_school?retryWrites=true&w=majority' ;
+const uri = process.env.MONGODB_URI;
 
 if (!uri) {
   throw new Error('Please add your MongoDB URI to .env');
